93e463cc68fe3b015adf5b85e7d11b0a7d62250e
Tarun Kumar <sinnytk@github.com>
2019-09-21 11:41:40 +0500

    React Boilerplate

---
+++ modulartable-react/src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Header} from './components/layout/Header'
+export const App = () => (
+  <Header>
+  </Header>
+);
---
+++ modulartable-react/src/components/layout/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Logo from '../../media/logo.png';
+export const Header = () => {
+    const x = 1;
+    return <header className="header">
+        <nav>
+            I am header
+        </nav>
+    </header>
+}
