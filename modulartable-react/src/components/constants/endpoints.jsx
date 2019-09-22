4ff81df7f06463d987e9685d55a872f38b18dbe5
Tarun Kumar <sinnytk@github.com>
2019-09-22 11:51:11 +0500

    Base setup of slots

---
+++ modulartable-react/src/components/constants/endpoints.jsx
@@ -0,0 +1,9 @@
+const BASE_URL = "http://127.0.0.1:8000";
+
+//timetable specific endpoints
+export const TIMETABLE_ENDPOINT = `${BASE_URL}/timetable`;
+
+//query specific endpoints
+export const SLOTS_ENDPOINT = `${BASE_URL}/slots?`;
+export const TEACHERS_ENDPOINT = `${BASE_URL}/teachers?`;
+export const SECTIONS_ENDPOINT = `${BASE_URL}/sections?`;
--- modulartable-react/src/components/content/TimetableCreate.jsx
+++ modulartable-react/src/components/content/TimetableCreate.jsx
@@ -1,12 +1,49 @@
 import React, { Component } from "react";
-
+import axios from "axios";
+import { SLOTS_ENDPOINT } from "../constants/endpoints";
 export default class TimetableCreate extends Component {
-  state = {};
+  state = {
+    timeslots: null,
+    days: null,
+    slots: []
+  };
+  componentDidMount() {
+    console.log(this.state.firstTime);
+    axios
+      .get(SLOTS_ENDPOINT)
+      .then(response => {
+        const slots = response.data;
+        this.setState({ slots });
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
+  }
   render() {
+    const slots = this.state.slots;
     return (
-      <div>
-        <h1>Timetable Create</h1>
-      </div>
+      <ul>
+        {slots.map(slot => (
+          <li
+            key={`${slot.daynum}-${slot.venue.venuenum}-${slot.timeslot.timeslotnum}`}
+          >
+            <div>
+              <p>Venue: {slot.venue.venuename}</p>
+              <p>
+                Timeslot:
+                {`${slot.timeslot.starttime} - ${slot.timeslot.endtime}`}
+              </p>
+              {slot.teacher && <p>Teacher: {slot.teacher.teachername}</p>}
+              {slot.section && (
+                <p>
+                  Section:{`${slot.section.semester}${slot.section.section}`}
+                </p>
+              )}
+              {slot.course && <p>Course: {slot.course.coursecode}</p>}
+            </div>
+          </li>
+        ))}
+      </ul>
     );
   }
 }
