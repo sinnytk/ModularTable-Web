a3dffd75d24302f8ae18928ddfec996d7b9159c5
Tarun Kumar <sinnytk@github.com>
2019-09-26 11:10:21 +0500

    Save child props and use table for data

--- modulartable-react/src/App.js
+++ modulartable-react/src/App.js
@@ -5,16 +5,24 @@ import TimetableCreate from "./components/content/TimetableCreate";
 import TimetableQuery from "./components/content/TimetableQuery";
 
 export class App extends Component {
-  state = { activeTabIndex: 1 };
+  state = { activeTabIndex: 1, slots: null };
   navigate = navigatedTo => {
     this.setState({ activeTabIndex: navigatedTo });
+  };
+  updateSlotsFromChild = slots => {
+    this.setState({ slots: slots.slots });
   };
   render() {
     const tabIndex = this.state.activeTabIndex;
     return (
       <div>
         <Header navigateParent={this.navigate} />
-        {tabIndex === 0 && <TimetableCreate />}
+        {tabIndex === 0 && (
+          <TimetableCreate
+            slots={this.state.slots}
+            updateSlots={this.updateSlotsFromChild}
+          />
+        )}
         {tabIndex === 1 && <Home />}
         {tabIndex === 2 && <TimetableQuery />}
       </div>
--- modulartable-react/src/components/content/TimetableCreate.jsx
+++ modulartable-react/src/components/content/TimetableCreate.jsx
@@ -2,48 +2,51 @@ import React, { Component } from "react";
 import axios from "axios";
 import { TIMETABLE_ENDPOINT } from "../constants/endpoints";
 import Slot from "./Timetable/Slot";
-import Grid from "@material-ui/core/Grid";
+import { Paper } from "@material-ui/core";
+import "./TimetableCreate.css";
 
 export default class TimetableCreate extends Component {
   state = {
     timeslots: null,
     days: null,
-    slots: null,
+    slots: this.props.slots,
     selectedDay: 1
   };
   componentDidMount() {
-    axios
-      .get(TIMETABLE_ENDPOINT)
-      .then(response => {
-        const slots = response.data;
-        this.setState({ slots });
-      })
-      .catch(error => {
-        console.log(error.response);
-      });
+    if (!this.state.slots) {
+      axios
+        .get(TIMETABLE_ENDPOINT)
+        .then(response => {
+          const slots = response.data;
+          this.setState({ slots });
+          this.props.updateSlots({ slots });
+        })
+        .catch(error => {
+          console.log(error.response);
+        });
+    }
   }
   render() {
     const slots = this.state.slots;
-    {
-      console.log(slots);
-    }
     const selectedDay = this.state.selectedDay;
-
     return (
-      <div>
-        <Grid container direction="row" justify="center">
+      <div style={{ width: "100%", height: "100%" }}>
+        <table>
           {slots &&
-            Object.entries(slots[selectedDay]).map(keyValue =>
-              keyValue[1].map(slot => (
-                <Grid
-                  key={`${slot.daynum}-${slot.timeslot.timeslotnum}-${slot.venuenum}`}
-                  item
-                >
-                  <Slot attributes={slot} />
-                </Grid>
-              ))
-            )}
-        </Grid>
+            Object.entries(slots[selectedDay]).map(keyValue => (
+              <tr>
+                <th className="slotVenue">{keyValue[0]}</th>
+                {keyValue[1].map(slot => (
+                  <td
+                    key={`${slot.daynum}-${slot.timeslot.timeslotnum}-${slot.venuenum}`}
+                    item
+                  >
+                    <Slot attributes={slot} />
+                  </td>
+                ))}
+              </tr>
+            ))}
+        </table>
       </div>
     );
   }
