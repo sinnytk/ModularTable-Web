84943b55a321e5bfd15b9c0533e1bd3ab9c19215
Tarun Kumar <sinnytk@github.com>
2019-09-29 20:48:02 +0500

    TimetableCreate customization options support

--- modulartable-react/src/components/content/Timetable/TimetableForm.jsx
+++ modulartable-react/src/components/content/Timetable/TimetableForm.jsx
@@ -1,30 +1,164 @@
-import React from "react";
-import { FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
+import React, { useState, Fragment } from "react";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  makeStyles,
+  Menu,
+  Button
+} from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120
+  }
+}));
+
 const TimetableForm = props => {
+  const classes = useStyles();
+
+  const teachers = props.teachers;
+  const sections = props.sections;
+  const courses = props.courses;
+  const [selectedTeachers, setSelectedTeachers] = useState(
+    props.selectedTeachers
+  );
+  const [selectedCourses, setSelectedCourses] = useState(props.selectedCourses);
+  const [selectedSections, setSelectedSections] = useState(
+    props.selectedSections
+  );
+
   const handleDayChange = event => {
     props.handleDayChange(event.target.value);
   };
+  const handleTeacherChange = event => {
+    let values = event.target.value;
+
+    if (values.length > 1) {
+      values[0] === 0 && (values = values.slice(1));
+      values[values.length - 1] === 0 && (values = [0]);
+    }
+    setSelectedTeachers(values);
+  };
+
+  const handleCourseChange = event => {
+    let values = event.target.value;
+
+    if (values.length > 1) {
+      values[0] === 0 && (values = values.slice(1));
+      values[values.length - 1] === 0 && (values = [0]);
+    }
+    setSelectedCourses(values);
+  };
+
+  const handleSectionChange = event => {
+    let values = event.target.value;
+
+    if (values.length > 1) {
+      values[0] === 0 && (values = values.slice(1));
+      values[values.length - 1] === 0 && (values = [0]);
+    }
+    setSelectedSections(values);
+  };
+  const submitChanges = () => {
+    props.handleMultipleChange({
+      selectedTeachers,
+      selectedCourses,
+      selectedSections
+    });
+  };
   return (
-    <form autoComplete="off">
-      <FormControl>
-        <InputLabel>Day</InputLabel>
-        <Select
-          value={props.selectedDay}
-          onChange={handleDayChange}
-          inputProps={{
-            name: "day",
-            id: "timetable-day"
-          }}
-        >
-          <MenuItem value={1}>Monday</MenuItem>
-          <MenuItem value={2}>Tuesday</MenuItem>
-          <MenuItem value={3}>Wednesday</MenuItem>
-          <MenuItem value={4}>Thursday</MenuItem>
-          <MenuItem value={5}>Friday</MenuItem>
-          <MenuItem value={6}>Saturday</MenuItem>
-        </Select>
-      </FormControl>
-    </form>
+    <Fragment>
+      <form autoComplete="off" className={classes.root}>
+        <FormControl className={classes.formControl}>
+          <InputLabel>Day</InputLabel>
+          <Select
+            value={props.selectedDay}
+            onChange={handleDayChange}
+            inputProps={{
+              name: "day",
+              id: "timetable-day"
+            }}
+          >
+            <MenuItem value={1}>Monday</MenuItem>
+            <MenuItem value={2}>Tuesday</MenuItem>
+            <MenuItem value={3}>Wednesday</MenuItem>
+            <MenuItem value={4}>Thursday</MenuItem>
+            <MenuItem value={5}>Friday</MenuItem>
+            <MenuItem value={6}>Saturday</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel>Teacher(s)</InputLabel>
+          <Select
+            multiple
+            value={selectedTeachers}
+            onChange={handleTeacherChange}
+            inputProps={{
+              name: "teacher(s)",
+              id: "timetable-teacher"
+            }}
+          >
+            <MenuItem value={0}>ALL</MenuItem>
+            {teachers &&
+              teachers.map(teacher => (
+                <MenuItem value={teacher.teachernum} key={teacher.teachernum}>
+                  {teacher.teachername}
+                </MenuItem>
+              ))}
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel>Courses(s)</InputLabel>
+          <Select
+            multiple
+            value={selectedCourses}
+            onChange={handleCourseChange}
+            inputProps={{
+              name: "course(s)",
+              id: "timetable-course"
+            }}
+          >
+            <MenuItem value={0}>ALL</MenuItem>
+            {courses &&
+              courses.map(course => (
+                <MenuItem value={course.coursenum} key={course.coursenum}>
+                  {course.coursecode}
+                </MenuItem>
+              ))}
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel>Section(s)</InputLabel>
+          <Select
+            multiple
+            value={selectedSections}
+            onChange={handleSectionChange}
+            inputProps={{
+              name: "section(s)",
+              id: "timetable-section"
+            }}
+          >
+            <MenuItem value={0}>ALL</MenuItem>
+            {sections &&
+              sections.map(section => (
+                <MenuItem value={section.sectionnum} key={section.sectionnum}>
+                  {`${section.semester}${section.section}`}
+                </MenuItem>
+              ))}
+          </Select>
+        </FormControl>
+      </form>
+      <Button variant="contained" color="primary" onClick={submitChanges}>
+        Create
+      </Button>
+    </Fragment>
   );
 };
 export default TimetableForm;
--- modulartable-react/src/components/content/TimetableCreate.jsx
+++ modulartable-react/src/components/content/TimetableCreate.jsx
@@ -9,6 +9,18 @@ export default class TimetableCreate extends Component {
   state = {
     timeslots: null,
     slots: this.props.slots,
+    teachers: this.props.teachers,
+    sections: this.props.sections,
+    courses: this.props.courses,
+    selectedTeachers: this.props.selectedTeachers
+      ? this.props.selectedTeachers
+      : [0],
+    selectedSections: this.props.selectedSections
+      ? this.props.selectedSections
+      : [0],
+    selectedCourses: this.props.selectedCourses
+      ? this.props.selectedCourses
+      : [0],
     selectedDay: 1
   };
   componentDidMount() {
@@ -16,8 +28,16 @@ export default class TimetableCreate extends Component {
       axios
         .get(TIMETABLE_ENDPOINT)
         .then(response => {
-          const slots = response.data;
-          this.setState({ slots });
+          const slots = response.data["slots"];
+          const teachers = response.data["teachers"];
+          const sections = response.data["sections"];
+          const courses = response.data["courses"];
+          this.setState({
+            slots,
+            teachers,
+            sections,
+            courses
+          });
           this.props.updateSlots({ slots });
         })
         .catch(error => {
@@ -28,9 +48,48 @@ export default class TimetableCreate extends Component {
   handleDayChange = selectedDay => {
     this.setState({ selectedDay });
   };
+  handleMultipleChange = selectedParams => {
+    const {
+      selectedTeachers,
+      selectedCourses,
+      selectedSections
+    } = selectedParams;
+    const teacherQuery =
+      selectedTeachers[0] !== 0
+        ? `teacherNum=${selectedTeachers.toString()}`
+        : "";
+    const courseQuery =
+      selectedCourses[0] !== 0 ? `courseNum=${selectedCourses.toString()}` : "";
+    const sectionQuery =
+      selectedSections[0] !== 0
+        ? `sectionNum=${selectedSections.toString()}`
+        : "";
+    axios
+      .get(
+        `${TIMETABLE_ENDPOINT}?${teacherQuery}&${courseQuery}&${sectionQuery}`
+      )
+      .then(response => {
+        const slots = response.data["slots"];
+        this.setState({
+          slots,
+          selectedTeachers,
+          selectedCourses,
+          selectedSections
+        });
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
+  };
   render() {
     const slots = this.state.slots;
     const selectedDay = this.state.selectedDay;
+    const teachers = this.state.teachers;
+    const sections = this.state.sections;
+    const courses = this.state.courses;
+    const selectedTeachers = this.state.selectedTeachers;
+    const selectedSections = this.state.selectedSections;
+    const selectedCourses = this.state.selectedCourses;
     return (
       <Fragment>
         <div className="timetable">
@@ -67,8 +126,15 @@ export default class TimetableCreate extends Component {
         </div>
         <div className={slots ? "TimetableForm" : "hidden"}>
           <TimetableForm
-            selectedDay={this.state.selectedDay}
+            teachers={teachers}
+            sections={sections}
+            courses={courses}
+            selectedDay={selectedDay}
+            selectedTeachers={selectedTeachers}
+            selectedSections={selectedSections}
+            selectedCourses={selectedCourses}
             handleDayChange={this.handleDayChange}
+            handleMultipleChange={this.handleMultipleChange}
           />
         </div>
       </Fragment>
