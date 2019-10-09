6a5e0f53823a214be12b65de07359fbca9a20ed2
Tarun Kumar <sinnytk@github.com>
2019-10-10 01:08:48 +0500

    Temp hosting to pythonanywhere

--- modulartable-react/src/components/constants/endpoints.jsx
+++ modulartable-react/src/components/constants/endpoints.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "http://sinnytk.pythonanywhere.com/";
 
 //timetable specific endpoints
 export const TIMETABLE_ENDPOINT = `${BASE_URL}/timetable`;
