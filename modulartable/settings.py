033d50c822e37bdfcf428c72f17ae0a690978f9d
Tarun Kumar <tarunkumar@pop-os.localdomain>
2019-10-16 21:05:16 +0500

    Update allowed_hosts

--- modulartable/settings.py
+++ modulartable/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['modulartable.herokuapp.com']
 
 
 # Application definition
